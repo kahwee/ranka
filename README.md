70bd934254841e9f92cf11236dceac2ca41bd01f
Kah Wee Teng <kahwee@Kahs-MacBook-Air.local>
2016-11-17 01:49:55 -0800

    Added README

---
+++ README.md
@@ -0,0 +1,24 @@
+# ranka
+
+Facebook Messaging bot with greater expressivity.
+
+```js
+const Ranka = require('ranka')
+const ranka = new Ranka({
+  serverURL: '',
+  validationToken: '',
+  pageAccessToken: ''
+})
+app.use('/webhook', Ranka.router({
+  ranka: ranka
+}))
+
+ranka.on('message', (req, res) => {
+  res
+    .sendText(`mm...`)
+    .typing()
+    .wait(3000)
+    .sendText(`Did you say "${req.body.message.text}"?`)
+    .exec()
+})
+```
--- src/response.js
+++ src/response.js
@@ -1,20 +1,58 @@
+const Promise = require('bluebird')
 class Response {
   constructor (opts) {
     this.req = opts.req
     this.ranka = opts.ranka
+    this.actions = []
+  }
+
+  wait (ms) {
+    this.actions.push(() => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, ms)
+    }))
+    return this
+  }
+
+  typing (isTyping = true) {
+    this.actions.push(() => {
+      return new Promise((resolve, reject) => {
+        this.ranka.api({
+          recipient: this.req.body.sender,
+          sender_action: isTyping ? 'typing_on' : 'typing_off'
+        }).then(resolve)
+      })
+    })
+    return this
   }
 
   send (message) {
-    this.ranka.callSendAPI({
-      recipient: this.req.body.sender,
-      message: message
+    this.actions.push(() => {
+      return new Promise((resolve, reject) => {
+        this.ranka.api({
+          recipient: this.req.body.sender,
+          message: message
+        }).then(resolve)
+      })
     })
+    return this
+  }
+
+  exec () {
+    this.actions.reduce(function (prev, curr) {
+      return prev.then(curr)
+    }, Promise.resolve())
+      .then(function (result) {
+        console.log('RESULT is ')
+      })
   }
 
   sendText (text) {
     this.send({
       text: text
     })
+    return this
   }
 
   sendImage (image) {
